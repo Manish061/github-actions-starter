e8ba7cc64d3c72fc9494a55316cf88f8d9f0144e
Manish Jha <mnshjha05@gmail.com>
2021-11-16 20:23:10 +0530

    add intersection and mutation observer

--- src/components/logs-app.ts
+++ src/components/logs-app.ts
@@ -1,16 +1,70 @@
-import { controller } from "@github/catalyst";
+import { controller, targets } from "@github/catalyst";
+import { LogsDetailsElement } from "./logs-details";
 
 @controller
 export class LogsAppElement extends HTMLElement {
   #max = 10;
+  #intersectionObserver: IntersectionObserver | null = null;
+  @targets details!: HTMLElement[];
   connectedCallback() {
     this.update();
+    this.setupIntersectionObservers();
+    this.addMutationObserver();
+  }
+
+  addMutationObserver() {
+    const callback: MutationCallback = (mutationRecord) => {
+      for (const record of mutationRecord) {
+        if (record.type === "childList") {
+          record.addedNodes.forEach((node) => {
+            const targetEl = node.parentElement?.querySelector("#showMore");
+            if (node.nodeName === "DETAILS") {
+              if (this.#intersectionObserver && targetEl) {
+                this.#intersectionObserver.observe(targetEl);
+              }
+            }
+          });
+        }
+      }
+    };
+    const ob = new MutationObserver(callback);
+    ob.observe(this, {
+      childList: true,
+      subtree: true,
+    });
+  }
+
+  removeIntersectionObserver() {
+    if (this.#intersectionObserver) {
+      this.#intersectionObserver.disconnect();
+      this.#intersectionObserver = null;
+    }
+  }
+
+  setupIntersectionObservers() {
+    this.removeIntersectionObserver();
+    this.#intersectionObserver = new IntersectionObserver((entries) => {
+      if (entries.length < 1) {
+        return;
+      }
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          (
+            entry.target.parentElement?.parentElement as LogsDetailsElement
+          )?.loadMoreContent();
+        }
+      });
+    }, {});
   }
 
   renderStepDetails(): string {
     const result: string[] = [];
     for (let i = 0; i < this.#max; i++) {
-      result.push(`<logs-details data-step=${i + 1}></logs-details>`);
+      result.push(
+        `<logs-details data-targets="logs-app.details" data-step=${
+          i + 1
+        }></logs-details>`
+      );
     }
     return result.join("");
   }
--- src/components/logs-details.ts
+++ src/components/logs-details.ts
@@ -1,4 +1,5 @@
 import { attr, controller } from "@github/catalyst";
+import faker from "../faker.js";
 
 @controller
 export class LogsDetailsElement extends HTMLElement {
@@ -8,13 +9,39 @@ export class LogsDetailsElement extends HTMLElement {
     this.update();
   }
 
+  loadMoreContent() {
+    const targetEl = this.querySelector(".log_description");
+    if (!targetEl) {
+      return;
+    }
+    const button = this.querySelector("button");
+    if (!button) {
+      return;
+    }
+    button.textContent = "Loading...";
+    setTimeout(() => {
+      const elementsToAdd: (Node | string)[] = faker.Lorem.sentences(50)
+        .split("\n")
+        .map((sentence: string): Node => {
+          const el = document.createElement("div");
+          el.textContent = sentence;
+          el.classList.add("log_line");
+          return el;
+        });
+      targetEl.append(...elementsToAdd);
+      button.textContent = "Show more";
+    }, 2000);
+  }
+
   update() {
     this.innerHTML = `
           <details class="log" id=${this.step}>
             <summary class="log_summary">Step ${this.step}</summary>
             <div class="log_description">
             </div>
-              <div style="text-align: center">Show more</div>
+            <div style="text-align: center" id="showMore">
+              <button style="padding: 10px" data-action="click:logs-details#loadMoreContent">Show more</button>
+              </div>
           </details>
         `;
   }
